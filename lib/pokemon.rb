bdf28047d02bc661635c4cf91706b48b564ff6cf
Nate Tan <nktan93@gmail.com>
2019-05-09 02:54:12 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,12 +9,12 @@ class Pokemon
     @db = db
   end
   
-  def self.save(name, type, hp, db)
+  def self.save(name, type, db)
     sql = <<-SQL
-      INSERT INTO pokemon (name, type, hp)
-      VALUES (?, ?, ?)
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
     SQL
-    db.execute(sql, name, type, hp)
+    db.execute(sql, name, type)
   end
   
   def self.find(id, db)
@@ -24,8 +24,7 @@ class Pokemon
   end
   
   def alter_hp(hp, db)
-    @hp = hp
     sql = "UPDATE pokemon SET hp = ? WHERE id = ?"
-    db.execute(sql, hp)
+    db.execute(sql, hp, self.id)
   end
 end
