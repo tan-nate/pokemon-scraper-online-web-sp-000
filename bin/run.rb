2dee73656ed5b679e279974693b8cbe7193bbd4d
Nate Tan <nktan93@gmail.com>
2019-05-09 03:37:35 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -7,10 +7,10 @@ SQLRunner.new(@db).execute_create_hp_column
 Pokemon.save('Pikachu', 'electric', @db)
 Pokemon.save('Magikarp', 'water', @db)
 
-bulbasaur = Pokemon.find(1, @db)
-
 bulbasaur.alter_hp(59, @db)
 
 pokemon = @db.execute("SELECT * FROM pokemon")
+
+bulbasaur = Pokemon.find(1, @db)
 
 puts pokemon
